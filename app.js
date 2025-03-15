5a96c4ce68682bb5b58b778adc750f4fb5dcda67
tabanroid <140057270+tabanroid@users.noreply.github.com>
2025-03-15 12:45:06 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const dataBase = require("./database");
-
+// افزودن
 dataBase.content("127.0.0.1", "root", "", "music");
-
-const read = async () => {
+// اتصال به دیتابیس
+const update = async () => {
   const res = await dataBase.handleData("UPDATE ai_users SET display_name = ? WHERE ID = ?", ["123", 1]);
   console.log(res["changedRows"]);
 };
-read();
+update();
